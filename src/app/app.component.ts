60e9f3ea78590a7a64d2e111b448ea523494529d
anasmak04 <anasdev8@gmail.com>
2022-10-01 16:54:01 +0100

    create a observable from rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,32 +6,32 @@ import { from, fromEvent } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent  {
   title = 'rxjs-fundamentals';
 
-  RandomUsers = [
-   {name : "anas", job : "Backend developer"},
-    {name : "bilal", job : "software enginner"},
-    {name : "anas elmakhloufi", job : "front-end developer"}
-  ]; 
+  // RandomUsers = [
+  //  {name : "anas", job : "Backend developer"},
+  //   {name : "bilal", job : "software enginner"},
+  //   {name : "anas elmakhloufi", job : "front-end developer"}
+  // ]; 
 
-  PostArrayObservable = from(this.RandomUsers);
+  // PostArrayObservable = from(this.RandomUsers);
 
-  constructor(){
-    this.PostArrayObservable.subscribe({
-      next : (data) => console.log(data),
-      error : (error) => console.log("error from rxjs :" +  error),
-      complete : () => console.log("complete data done!")
-    })
-  }
+  // constructor(){
+  //   this.PostArrayObservable.subscribe({
+  //     next : (data) => console.log(data),
+  //     error : (error) => console.log("error from rxjs :" +  error),
+  //     complete : () => console.log("complete data done!")
+  //   })
+  // }
 
 
-  ngAfterViewInit() {
-    fromEvent(document.getElementById('click-button')!, 'click').subscribe({
-      next : (data) => console.log(data),
-      error : (error) => console.log("error from rxjs :" +  error),
-      complete : () => console.log("complete data done!")
-    })
+  // ngAfterViewInit() {
+  //   fromEvent(document.getElementById('click-button')!, 'click').subscribe({
+  //     next : (data) => console.log(data),
+  //     error : (error) => console.log("error from rxjs :" +  error),
+  //     complete : () => console.log("complete data done!")
+  //   })
 
-  }
+  // }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { from, observable } from 'rxjs';
+import { from, Observable, observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +11,25 @@ export class HomeComponent implements OnInit {
   constructor( ) { }
 
   ngOnInit(): void {
+    const newObservable = new Observable<number>((observer) => {
+      for(let i = 0 ; i < 5 ; i ++){
+
+        
+        console.log(`number is  : ${observer.next(i)}`)
+
+      }
+
+      observer.complete();
+    })
+
+      let observer = {
+        next: (data : number) => console.log(data),
+        error: (error : string) => console.log(error),
+        complete : () => console.log("complete process data")
+      };
+      
+        newObservable.subscribe(observer)
   }
-
-
 
 
 }
