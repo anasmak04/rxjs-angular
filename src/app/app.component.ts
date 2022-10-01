bcdbedbef5477e4c4de89e83546c6b14b3818b8b
anasmak04 <anasdev8@gmail.com>
2022-10-01 16:28:36 +0100

    event handlers from rxjs and observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { from, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'rxjs-fundamentals';
+
+  RandomUsers = [
+   {name : "anas", job : "Backend developer"},
+    {name : "bilal", job : "software enginner"},
+    {name : "anas elmakhloufi", job : "front-end developer"}
+  ]; 
+
+  PostArrayObservable = from(this.RandomUsers);
+
+  constructor(){
+    this.PostArrayObservable.subscribe({
+      next : (data) => console.log(data),
+      error : (error) => console.log("error from rxjs :" +  error),
+      complete : () => console.log("complete data done!")
+    })
+  }
+
+
+  ngAfterViewInit() {
+    fromEvent(document.getElementById('click-button')!, 'click').subscribe({
+      next : (data) => console.log(data),
+      error : (error) => console.log("error from rxjs :" +  error),
+      complete : () => console.log("complete data done!")
+    })
+
+  }
 }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { from, observable } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  constructor( ) { }
+
+  ngOnInit(): void {
+  }
+
+
+
+
+}
